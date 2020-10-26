43e2ce45e457757acf98d625f76a87d047d96d2a
Luke Yeh <lukeoverrhere@gmail.com>
2020-10-26 12:48:35 -0400

    final question on chapter 3

--- chapters/3.hs
+++ chapters/3.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -- Rewrite the not' function from the previous chapter in pattern matching
 -- style
 
@@ -32,3 +34,13 @@ powGuarded :: (Ord b, Num b, Num a) => a -> b -> a
 
 powGuarded x n | n < 1     = 1
                | otherwise = x * powMatch x (n - 1)
+
+-- use guarded equations to write a function which categorises characters into
+-- 3 kinds: kind 0 for the lowercase letters a...z, kind 1 for the uppercase
+-- letters a...z, and kind 2 for everything else
+
+categoriseThree :: (Num a, Eq a) => Char -> a
+
+categoriseThree c | isLower c = 0
+                  | isUpper c = 1
+                  | otherwise = 2
